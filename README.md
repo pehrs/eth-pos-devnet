4cf6ba76817b916507fdbd82ffc7b30afe68ce19
islishude <islishude@gmail.com>
2024-05-15 10:41:20 +0800

    Update geth genesis script

--- README.md
+++ README.md
@@ -22,9 +22,29 @@ If you would like to fund address you have, just add `.env` file with following 
 
 ```
 # comma separated address
-GENESIS_ADDRESS=
-# funding amount
-GENESIS_AMOUNT=0xfffffffffffffffffffff
+GENESIS_ADDRESS=0x0001aEBC06288F578Eb01002a99E854cED86bC4F
+# the default balance
+GENESIS_BALANCE_default=0xfffffffffffffffffffff
+GENESIS_BALANCE_0x0001aEBC06288F578Eb01002a99E854cED86bC4F=0x64
+GENESIS_NONCE_0x0002538C7BB308B5042f279e2C10b466b80797C9=0x1
+GENESIS_CODE_0x0002538C7BB308B5042f279e2C10b466b80797C9=0x363d3d373d3d3d363d73bebebebebebebebebebebebebebebebebebebebe5af43d82803e903d91602b57fd5bf3
+```
+
+And the result will be
+
+```json
+{
+  "0001aebc06288f578eb01002a99e854ced86bc4f": {
+    "balance": "0x64"
+  },
+  "0002538c7bb308b5042f279e2c10b466b80797c9": {
+    "nonce": "0x1",
+    "code": "0x363d3d373d3d3d363d73bebebebebebebebebebebebebebebebebebebebe5af43d82803e903d91602b57fd5bf3"
+  },
+  "000352a341aac0a2437c33edceada93bf4908fd8": {
+    "balance": "0xfffffffffffffffffffff"
+  }
+}
 ```
 
 ```
---
+++ genesis/index.mjs
@@ -0,0 +1,93 @@
+import fs from "node:fs/promises";
+import { env } from "node:process";
+
+let defaultBalance = env.GENESIS_BALANCE_default;
+try {
+  defaultBalance = "0x" + BigInt(defaultBalance).toString(16);
+  console.log("default funding amount", defaultBalance);
+} catch {
+  console.log("invalid amount", defaultBalance);
+  process.exit(1);
+}
+
+const genesisFile = await fs.readFile("/config/genesis.json");
+
+const genesis = JSON.parse(genesisFile.toString());
+
+const alloc = {};
+
+for (const [address, value] of Object.entries(genesis.alloc || {})) {
+  if (address.startsWith("0x")) {
+    address = address.slice(2);
+  }
+  alloc[address.toLowerCase()] = value;
+}
+
+for (const [envName, envValue] of Object.entries(env)) {
+  if (!envName.startsWith("GENESIS")) {
+    continue;
+  }
+
+  let [_, kind, address] = envName.split("_");
+  if (!address || address === "default") {
+    continue;
+  }
+
+  if (!/^(0x)?[0-9a-fA-F]{40}$/.test(address)) {
+    continue;
+  }
+
+  kind = kind.toUpperCase();
+  address = address.toLowerCase();
+
+  if (address.startsWith("0x")) {
+    address = address.slice(2);
+  }
+
+  const cur = alloc[address] || {};
+
+  switch (kind) {
+    case "BALANCE":
+      try {
+        cur.balance = "0x" + BigInt(envValue).toString(16);
+      } catch {
+        cur.balance = defaultBalance;
+      }
+      console.log("address", address, "balance", cur.balance);
+      break;
+    case "CODE":
+      cur.code = envValue;
+      console.log("address", address, "code", cur.code);
+      break;
+    case "NONCE":
+      cur.nonce = envValue;
+      try {
+        cur.nonce = "0x" + BigInt(envValue).toString(16);
+        console.log("address", address, "nonce", cur.nonce);
+      } catch {
+        console.log("invalid nonce", envValue, "for address", address);
+      }
+      break;
+  }
+
+  alloc[address] = cur;
+}
+
+for (let addr of env.GENESIS_ADDRESS?.split(",")) {
+  if (/^(0x)?[0-9a-fA-F]{40}$/.test(addr)) {
+    if (addr.startsWith("0x")) {
+      addr = addr.slice(2);
+    }
+
+    addr = addr.toLowerCase();
+
+    const cur = alloc[addr] || {};
+    cur.balance = defaultBalance;
+    alloc[addr] = cur;
+  }
+}
+
+if (Object.keys(alloc).length > Object.keys(genesis.alloc).length) {
+  genesis.alloc = alloc;
+  await fs.writeFile("/geth/genesis.json", JSON.stringify(genesis, null, "  "));
+}
